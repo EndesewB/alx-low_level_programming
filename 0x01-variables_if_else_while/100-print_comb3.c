8136c5f0317e3cf2d3dbe5fa396100efb4c12538
endesew yenesew <endesew12@gmail.com>
2022-12-09 06:04:52 -0800

    prints all two digit numbers except same digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,17 +2,17 @@
 /**
  * main - Entry point
  *
- * Return: 0 
+ * Return: 0
  */
 int main(void)
 {
 	int i, j;
 
-	for (i = 48;i < 58; i++)
+	for (i = 48; i < 58; i++)
 	{
 		for (j = i; j < 58; j++)
 		{
-			if(i == j)
+			if (i == j)
 			{
 				continue;
 			}

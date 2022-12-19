513a2c877d0c2c8d7d46358aedd18d38634de0a9
endesew yenesew <endesew12@gmail.com>
2022-12-19 02:25:31 -0800

    pointers arrays and strings prototype

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,12 +6,6 @@
  */
 void reset_to_98(int)
 {
-	int n;
 
-	n = 402;
-	_putchar("n = %d\n", n);
-	reset_to_98(&n);
-	_putchar("n = %d\n", n);
-	return (0);
+	*n = 98;
 }
-
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
@@ -9,6 +10,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _putchar(char c)
+int _putchar(char c);
 void reset_to_98(int *n);
 #endif

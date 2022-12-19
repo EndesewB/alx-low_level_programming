c304d66f0b4dc52e27e09b1d7b1fa31e1df3962b
endesew yenesew <endesew12@gmail.com>
2022-12-19 04:24:01 -0800

    a function that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+*/
+
+void rev_string(char *s)
+{
+	int j, i;
+	char chr;
+
+	for (j = 0; s[j] != '\0'; ++j)
+		;
+	for (i = 0; i < j / 2; ++i)
+	{
+		chr = s[i];
+		s[i] = s[l - 1 - i];
+		s[j - 1 - i] = chr;
+	}
+
+}

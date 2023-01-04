ae9803e7fced613457b2bb4f3f99e7e818407dd0
Endesew Bantayehu Yenesew <endesew12@gmail.com>
2023-01-04 00:49:46 -0800

    a function that returns the natural square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * sqrt2 - the square root of number
+ * @a: fist square root value
+ * @b: the number that go to sqrts
+ *
+ * Return: value 
+ */
+int sqrt2(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (sqrt2(a, b + 1));
+}
+/**
+ * _sqrt_recursion - self calling
+ * @n: an integer number
+ *
+ * Return: nothing
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt2(n, 1));
+}

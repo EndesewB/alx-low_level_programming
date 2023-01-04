7a121668f643bca343efa29134bb3ffc52b31446
Endesew Bantayehu Yenesew <endesew12@gmail.com>
2023-01-04 00:08:51 -0800

    header files

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,7 @@
 #ifndef _PUTCHAR_H
 #define _PUTCHAR_H
 int _putchar(char c);
-void _puts_recursion(char *s)
+void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);

badaa868293fdcd6bf9f52c4a2dca35b31b7754c
Endesew Bantayehu Yenesew <endesew12@gmail.com>
2023-01-06 16:08:55 -0800

    a program that prints the number of arguments passed into it

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,10 @@
 /**
  * main - Print the name of the program
  * @argc: Count arguments
- *
  * @argv: Arguments
- *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv[])
 {
 	(void) argc;
 

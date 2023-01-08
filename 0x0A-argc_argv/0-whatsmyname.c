25e0f414b60fc51bd4b751120fc41f10d4be58f4
Endesew Bantayehu Yenesew <endesew12@gmail.com>
2023-01-08 12:53:39 -0800

    a program that prints its name, followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 /**
  * main - print the name of the file followed by a new line
  * @argc: argument count
@@ -8,8 +8,8 @@
  */
 int main(int argc, char *argv[])
 {
-	(void) argc;
 	printf("%s\n", argv[0]);
-	return (0);
-
+	(void)argc;
+	return (0);	
 }
+

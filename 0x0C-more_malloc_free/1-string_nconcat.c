7462e5bf2686964966ff09819349cce593da2114
Endesew Bantayehu Yenesew <endesew12@gmail.com>
2023-01-12 13:33:24 -0800

    Write a function that allocates memory using malloc.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,60 +1,47 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *_slen - string length
- *@s: input string
- *
- *Return: returns length
- */
-int _slen(char *s)
-{
-        int i = 0;
-
-        while (s[i] != 0)
-        {
-                i++;
-        }
-        return (i);
-}
-/**
- * string_nconcat - concatenates twwo strings
- * @s1: destination string
- * @s2: sourse string
- * @n: number of characters
- * Return: sn On success
+ * string_nconcat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: amount of bytes.
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-        char *sn;
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
-        int i, j, slen1, slen2;
-        if (s1 == NULL)
-        {
-                s1 = "";
-        }
-        if (s2 == NULL)
-        {
-                s2 = "";
-        }
-        slen1 = _slen(s1);
-        slen2 = _slen(s2);
-        if (n >= slen2)
-        {
-                n = slen2;
-        }
-        sn = malloc(1 + sizeof(char) * (slen1 + n));
-        if (sn == NUUL)
-        {
-                return (NULL);
-        }
-        for (i = 0; s1[i] != 0; i++)
-        {
-                sn[i] = s1[i];
-        }
-        for (j = 0; s2[j] != 0 && j != n; j++)
-        {
-                sn[i] = s2[j];
-                i++;
-        }
-return (sn)
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
+
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
+
+	if (n > ls2)
+		n = ls2;
+
+	lsout = ls1 + n;
+
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
+		return (NULL);
+
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
+
+	sout[i] = '\0';
+
+	return (sout);
 }

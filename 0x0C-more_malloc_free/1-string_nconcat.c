614875173dc5124855640ebccfa93a23e785fdc6
Endesew Bantayehu Yenesew <endesew12@gmail.com>
2023-01-12 12:51:10 -0800

    Calloc memory  allocation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,13 +8,13 @@
  */
 int _slen(char *s)
 {
-	int i = 0;
+        int i = 0;
 
-	while (s[i] != 0)
-	{
-		i++;
-	}
-	return (i);
+        while (s[i] != 0)
+        {
+                i++;
+        }
+        return (i);
 }
 /**
  * string_nconcat - concatenates twwo strings
@@ -25,40 +25,36 @@ int _slen(char *s)
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *sn;
+        char *sn;
 
-	int i, j, slen1, slen2;
-
-	slen1 = _slen(s1);
-	slen2 = _slen(s2);
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	if (n >= slen2)
-	{
-		sn = malloc(1 + sizeof(char) * (slen1 + slen2));
-	}
-	else
-	{
-		sn = malloc(1 + sizeof(char) * (slen1 + n));
-	}
-	if (sn == NUUL)
-	{
-		return (NULL);
-	}
-	for (i = 0; s1[i] != 0; i++)
-	{
-		sn[i] = s1[i];
-	}
-	for (j = 0; s2[j] != 0 && j != n; j++)
-	{
-		sn[i] = s2[j];
-		i++;
-	}
-	return (sn)
+        int i, j, slen1, slen2;
+        if (s1 == NULL)
+        {
+                s1 = "";
+        }
+        if (s2 == NULL)
+        {
+                s2 = "";
+        }
+        slen1 = _slen(s1);
+        slen2 = _slen(s2);
+        if (n >= slen2)
+        {
+                n = slen2;
+        }
+        sn = malloc(1 + sizeof(char) * (slen1 + n));
+        if (sn == NUUL)
+        {
+                return (NULL);
+        }
+        for (i = 0; s1[i] != 0; i++)
+        {
+                sn[i] = s1[i];
+        }
+        for (j = 0; s2[j] != 0 && j != n; j++)
+        {
+                sn[i] = s2[j];
+                i++;
+        }
+return (sn)
 }

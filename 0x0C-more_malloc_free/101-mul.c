289cec426f76b80c0d3c333ce411854feb3444ac
Endesew Bantayehu Yenesew <endesew12@gmail.com>
2023-01-15 04:46:35 -0800

    correction for multiplication function

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -78,7 +78,8 @@ return (ln);
  */
 int main(int argc, char *argv[])
 {
-int ln1, ln2, lnout, add, addl, i, j, k, ca;char *nout;
+int ln1, ln2, lnout, add, addl, i, j, k, ca;
+char *nout;
 
 if (argc != 3)
 printf("Error\n"), exit(98);
@@ -104,7 +105,8 @@ i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
 if (j < 0)
 {
 if (nout[0] != '0')
-break;lnout--;
+break;
+lnout--;
 free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
 }
 if (j >= 0)
@@ -113,5 +115,6 @@ add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
 addl = add / 10, nout[k] = (add % 10) + '0';
 }
 }
-printf("%s\n", nout), free(nout);return (0);
+printf("%s\n", nout), free(nout);
+return (0);
 }

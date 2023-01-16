8b87c7fd9ba96920138aca5df0ed17390457073a
Endesew Bantayehu Yenesew <endesew12@gmail.com>
2023-01-16 07:45:44 -0800

    dog data

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,13 +6,13 @@
  * @age: age of the dog
  * @owner: to whom it belongs to
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/*dog_t *new_dog(char *name, float age, char *owner);*/
+dog_t *new_dog(char *name, float age, char *owner);
 #endif

4f6086ad17cf7ef8ce10c4a5271626089f9a9daa
Endesew Bantayehu Yenesew <endesew12@gmail.com>
2023-01-18 01:25:58 -0800

    a function that writes name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,12 +3,12 @@
 /**
  * print_name - prints supplyed by another function
  * @name: the name to be print
- * @f: the function to call to print the name
+ * @fun: the function to call to print the name
  *
  */
 void print_name(char *name, void (*fun)(char *))
 {
 	if (f == NULL)
 		return;
-	f(name);
+	fun(name);
 }

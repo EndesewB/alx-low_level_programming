1bf75809240203a0fc34100f0e93a949f8c37905
Endesew Bantayehu Yenesew <endesew12@gmail.com>
2023-01-19 10:05:03 -0800

    a function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,88 +1,53 @@
 #include "variadic_functions.h"
-#include<stdio.h>
-#include<stdarg.h>
-/**
- * p_char - print char
- * @list:arg
- * Return: void
- */
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-void p_char(va_list list)
-{
-	printf("%c", va_arg(list, int));
-}
 /**
- * p_string - print string
- * @list:arg
- * Return: void
- */
-
-void p_string(va_list list)
-{
-	char *str;
-
-	str = va_arg(list, char*);
-	if (str == NULL)
-		str = "(nil)";
-printf("%s", str);
-}
-/**
- * p_integer - print integer
- * @list:arg
- * Return: void
- */
-
-void p_integer(va_list list)
-{
-	printf("%i", va_arg(list, int));
-}
-/**
- * p_float - print float
- * @list:arg
- * Return: void
- */
-
-void p_float(va_list list)
-{
-	printf("%f", va_arg(list, double));
-}
-/**
- * print_all - print everything
- * @format:arg
- * Return: void
+ * print_all - A function prints anything.
+ * @format: A list of type of arguments passed to the function.
+ * Return: Nothing
  */
 void print_all(const char * const format, ...)
-
 {
-	unsigned int i, j;
-	t_print t[] = {
-		{"c", p_char},
-		{"s", p_string},
-		{"i", p_integer},
-		{"f", p_float},
-		{NULL, NULL}
-	};
-	va_list valist;
-	char *s = "";
+	va_list ap;
+	char *temp;
+	int i = 0;
 
-	va_start(valist, format);
-	i = 0;
-	while (format && format[i])
+	va_start(ap, format);
+	while (format == NULL)
 	{
-		j = 0;
-		while (t[j].x != NULL)
+		printf("\n");
+		return;
+	}
+	while (format[i])
+	{
+		switch (format[i])
 		{
-			if (*(t[j].x) == format[i])
+		case 'c':
+			printf("%c", (char) va_arg(ap, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(ap, int));
+			break;
+		case 'f':
+			printf("%f", (float) va_arg(ap, double));
+			break;
+		case 's':
+			temp = va_arg(ap, char*);
+			if (temp != NULL)
 			{
-				printf("%s", s);
-				t[j].T_func(valist);
-				s = ", ";
+				printf("%s", temp);
 				break;
 			}
-			j++;
+			printf("(nil)");
+			break;
 		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+		     format[i] == 's') && format[(i + 1)] != '\0')
+			printf(", ");
 		i++;
 	}
-	va_end(valist);
+	va_end(ap);
 	printf("\n");
 }

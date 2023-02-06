6b1e5c73c837e7cd1a27f5a521ce16864847fdf6
endesew Bantayehu <endesew12@gmail.com>
2023-02-06 09:07:57 -0800

    a program that displays the information contained in the ELF header at the start of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,21 +1,3 @@
-
-samibirhanu1
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-1
-Actions
-Projects
-Security
-Insights
-alx-low_level_programming/0x15-file_io/100-elf_header.c
-@samibirhanu1
-samibirhanu1 all tasks before README.md
- 1 contributor
-314 lines (286 sloc)  6.69 KB
 #include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
